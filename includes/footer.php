3145e3298265c107590e7879a2f3d9e6098583da
Mohit Singh Rajput <67924939+mohitrajput2002@users.noreply.github.com>
2023-08-25 12:55:53 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,51 @@
+<div class="footer">
+    <div class="page-container footer-container">
+        <div class="footer-cities">
+            <div class="footer-city">
+                <a href="property_list.php?city=Delhi">PG in Delhi</a>
+            </div>
+            <div class="nav-vl" style="color: white;">&nbsp;&nbsp;&nbsp;&nbsp;</div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Mumbai">PG in Mumbai</a>
+            </div>
+            <div class="nav-vl">&nbsp;&nbsp;&nbsp;&nbsp;</div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Bengaluru">PG in Bangalore</a>
+            </div>
+            <div class="nav-vl">&nbsp;&nbsp;&nbsp;&nbsp;</div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Hyderabad">PG in Hyderabad</a>
+            </div>
+            <div class="nav-vl">&nbsp;&nbsp;&nbsp;&nbsp;</div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Chennai">PG in Chennai</a>
+            </div>
+            <div class="nav-vl">&nbsp;&nbsp;&nbsp;&nbsp;</div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Ahmedabad">PG in Ahmedabad</a>
+            </div>
+            <div class="nav-vl">&nbsp;&nbsp;&nbsp;&nbsp;</div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Pune">PG in Pune</a>
+            </div>
+            <div class="nav-vl">&nbsp;&nbsp;&nbsp;&nbsp;</div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Lucknow">PG in Lucknow</a>
+            </div>
+            <div class="nav-vl">&nbsp;&nbsp;&nbsp;&nbsp;</div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Jaipur">PG in Jaipur</a>
+            </div>
+            <div class="nav-vl">&nbsp;&nbsp;&nbsp;&nbsp;</div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Chandigarh">PG in Chandigarh</a>
+            </div>
+
+        </div>
+        <div class="footer-copyright">&copy; 2023 Copyright PG Life </div>
+    </div>
+</div>
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/common.js"></script>
